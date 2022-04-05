fd5d40fb5516345ae648f7265c67c0a4c37a17f4
Shane Neff <shaneneff@microsoft.com>
2022-04-05 12:29:37 -0400

    moved from gh to local template

--- deployToSub.ps1
+++ deployToSub.ps1
@@ -1,1 +1,1 @@
-New-AzDeployment -Location "eastus2" -TemplateFile "https://raw.githubusercontent.com/nanigan/asc-standard-demo/main/ASC-Settings/azuredeploy.json"
+New-AzDeployment -Location "eastus2" -TemplateFile "ASC-Settings/azuredeploy.json"
